bce63ba25a8ba339ea729abfaa50d9e2c7b91e7d
Ranima <raphaelnmastel3@gmail.com>
2019-02-28 19:55:30 -0800

    Im re learning git

---
+++ CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    // how much the charcter will move by.
+    private Vector3 move;
+
+
+	// Use this for initialization
+	void Start () {
+        move = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    // Takes care of
+    void Movement()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.z = Input.GetAxis("Vertical");
+        transform.Translate(move);
+    }
+}
